3a00229292276fa611150a1f844fe4c35361fdcd
bachthyaglx <bachthy94@gmail.com>
2025-03-30 20:19:28 +0200

    Rebase repository

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <section className="min-h-screen px-6 py-20 bg-gradient-to-r from-gray-900 to-black">
+      <h2 className="text-4xl font-bold mb-10">Projects</h2>
+      
+    </section>
+  );
+}
